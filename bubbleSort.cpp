7e34ddbf988930ded0138b7f26eb69e5f64563e7
malxlip <malcolmlipscomb@Agent-14.local>
2014-07-09 17:59:24 -0400

    Comments

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 int main(){
 	int arrayLength = 8; // Magic number for the array to sort.
-	bool swap = true;
-	int swapBit = 0;
+	bool swap = true; // check to see if the sort needs to continue
+	int swapBit = 0; // keeps from having to add another loop for a sort check
 	int bSortArray[] = {14, -8, 2, 15, 6, -5, 1, 4};
 	int temp = 0;
-	
 	while(swap){
-		swapBit = 0;
+		swapBit = 0; 								// check if a swap happened
 		for (int j = 0; j < arrayLength; j++){
-			if(bSortArray[j] > bSortArray[j + 1]){
-				temp = bSortArray[j + 1];
+			if(bSortArray[j] > bSortArray[j + 1]){  // if not in order
+				temp = bSortArray[j + 1];			// move the bigger num to the right.
 				bSortArray[j + 1] = bSortArray[j];
 				bSortArray[j] = temp;
-				swapBit = 1;
+				swapBit = 1;						// if swap happened, gotta go again.
 			}
 		}
-		if (swapBit == 0) swap = false;
+		if (swapBit == 0) swap = false; // no swap happens, end While Loop
 	}
 	cout << endl;
 	for (int print = 0; print < arrayLength; print++){
